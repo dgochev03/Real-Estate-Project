f77ab515809899f9f7c1b90997cfef51c675242b
ZdravkoPT <zdravkoxd@gmail.com>
2025-03-04 15:45:19 +0200

    Page done

--- index.php
+++ index.php
@@ -41,33 +41,6 @@ include("config.php");
         </section>
 
         <section class="recent-properties">
-            <div class="container">
-                <h2>Recent Property</h2>
-                <div class="property-list">
-                    <?php
-                    $query = mysqli_query($con, "SELECT property.*, user.uname, user.utype FROM property, user WHERE property.uid = user.uid ORDER BY date DESC LIMIT 9");
-                    while ($row = mysqli_fetch_array($query)) {
-                        ?>
-
-                        <article class="property-item">
-                            <img src="images/property/<?php echo $row['12']; ?>" alt="property image">
-                            <div class="property-info">
-                                <h3><a id="linkstoads" href="propertydetail.php?pid=<?php echo $row['0']; ?>"><?php echo $row['1']; ?></a></h3>
-                                <p><?php echo $row['11']; ?></p>
-                                <ul>
-                                    <li> For <?php echo $row['5']; ?></li>
-                                    <li><?php echo $row['8']; ?> Sq.m.</li>
-                                    <li><?php echo $row['7']; ?> Floor</li>
-                                </ul>
-                                <div class="meta">
-                                    <span>By: <?php echo $row['uname']; ?></span>
-                                    <span><?php echo $row['9']; ?>  EUR</span>
-                                </div>
-                            </div>
-                        </article>
-                    <?php } ?>
-                </div>
-            </div>
         </section>
 
         <?php include("include/footer.php"); ?>
